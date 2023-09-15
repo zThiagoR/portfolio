4da500fdd76df866c229dd3c3797db341b4f533c
zThiagoR <thi.magalha13@gmail.com>
2023-09-14 23:14:23 -0300

    config: add  fonts and colors

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        "quantico": ["Quantico", "sans-serif"],
+        "roboto": ["Roboto", "sans-serif"],
+        "economica": ["Economica", "sans-serif"],
+      },
+      colors: {
+        "primary": "#203c52",
+        "secondary": "##31346a",
+        "tertiary": "#3a3f7a",
+      }
+    },
+  },
+  plugins: [],
+}
+
