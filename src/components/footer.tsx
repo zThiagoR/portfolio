741abcd3afb71ae2be4d014af0770d886763c8c4
zThiagoR <thi.magalha13@gmail.com>
2023-09-20 22:35:53 -0300

    create component footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="bg-primary p-4 text-center text-white">
+        <p>
+          Feito com 💙 por{' '}
+          <a href="https://github.com/zThiagoR" target="_blank" rel="noopener noreferrer">
+            Thiago M.
+          </a>
+        </p>
+    </footer>
+  )
+}
