cec5b58015a7bc9f5988afcb130e473edc1b2e22
zThiagoR <thi.magalha13@gmail.com>
2023-10-25 22:20:58 -0300

    Apply react-scroll to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-scroll';
+
 export default function Navbar() {
 
   const menus = ['Sobre mim', 'Tecnologias', 'Projetos']
@@ -6,7 +8,11 @@ export default function Navbar() {
     <nav className="flex justify-center bg-primary text-white md:justify-end">
       <ul className="flex items-center font-quantico font-bold sm:text-xl">
         {menus.map((menu, index) => (
-          <li key={index} className="cursor-pointer p-4 hover:bg-white hover:text-primary">{menu}</li>
+          <li key={index} className="cursor-pointer p-4 hover:bg-white hover:text-primary">
+            <Link to={menu} smooth={true} duration={500}>
+              {menu}
+            </Link>
+          </li>
         ))}
       </ul>
     </nav>
