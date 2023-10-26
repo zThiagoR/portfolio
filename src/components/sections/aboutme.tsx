f6ddca1095ace2553c97efe4d9d1d926c99e720f
zThiagoR <thi.magalha13@gmail.com>
2023-10-25 21:57:41 -0300

    Refactoring css to about me

--- src/components/sections/aboutme.tsx
+++ src/components/sections/aboutme.tsx
@@ -1,19 +1,19 @@
 export default function AboutMe() {
   return (
     <section id="aboutme" className="bg-secondary">
-      <div className="container mx-auto flex flex-col items-center justify-center md:flex-row-reverse md:h-screen">
+      <div className="container mx-auto flex flex-col items-center justify-center lg:h-screen lg:flex-row-reverse">
         <div>
           <img
             src="https://avatars.githubusercontent.com/u/56285370?v=4"
             alt=""
-            className="h-48 w-48 rounded-full md:h-64 md:w-64"
+            className="h-48 w-48 rounded-full lg:h-64 lg:w-64"
           />
         </div>
         <div className="mx-auto max-w-screen-md p-8 text-center text-white">
           <div className="text-4xl font-bold">Thiago M.</div>
           <span className="text-lg">FullStack Developer</span>
           <h2 className="mt-2 font-economica text-2xl font-medium">Quem sou eu?</h2>
-          <p>
+          <p className="mt-4 text-lg">
             Como desenvolvedor full-stack, minha paixão está em criar soluções web criativas e inovadoras. Tenho experiência tanto no desenvolvimento frontend quanto no backend, e meu foco é aprendizado contínuo e aprimoramento de minhas habilidades.
             <br />
             <br />
