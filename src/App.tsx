cda9df3a6a66b4815541a0f4d6007e1510fe928a
zThiagoR <thi.magalha13@gmail.com>
2023-10-25 21:56:29 -0300

    Apply all components to main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import Footer from "./components/footer";
 import Navbar from "./components/navbar";
+import AboutMe from "./components/sections/aboutme";
 import ProjectsSection from "./components/sections/projects";
+import StacksSection from "./components/sections/stacks";
 
 function App() {
   return (
-    <div className="flex min-h-screen flex-col bg-secondary">
-      <header className="bg-primary font-quantico text-white">
-        <Navbar />
-      </header>
-      <main className="grow">
-        <ProjectsSection />
-      </main>
+    <main className="flex min-h-screen flex-col bg-secondary text-white">
+      <Navbar />
+      <AboutMe />
+      <StacksSection />
+      <ProjectsSection />
       <Footer />
-    </div>
+    </main>
   );
 }
 
