73ca120b413c0e4c1ef612e44debb693676f4ecb
zThiagoR <thi.magalha13@gmail.com>
2023-12-29 15:22:06 -0300

    Add Python and Java skills to StacksSection

--- src/components/sections/stacks.tsx
+++ src/components/sections/stacks.tsx
@@ -1,36 +1,36 @@
 const skills = [
   { name: 'JavaScript', url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg' },
   { name: 'TypeScript', url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg' },
+  { name: 'Python', url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg' },
+  { name: 'Java', url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg' },
   { name: 'NodeJS', url: 'https://cdn-icons-png.flaticon.com/512/5968/5968322.png' },
   { name: 'Express', url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg' },
   { name: 'NestJS', url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nestjs/nestjs-plain.svg' },
   { name: 'React', url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg' },
-  { name: "Tailwind CSS", url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg' },
+  { name: "TailwindCSS", url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg' },
   { name: 'NextJS', url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg' },
   { name: 'MongoDB', url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg' },
   { name: 'PostgreSQL', url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg' },
   { name: 'Prisma', url: 'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/001/287/square_480/prismaHD.png' },
 ];
           
-export default function StacksSection(){
+export default function StacksSection() {
   return (
     <section id="Tecnologias" className="bg-tertiary">
       <div className="container m-auto">
         <h1 className="my-4 text-center font-quantico text-2xl font-bold">
           Minhas tecnologias
         </h1>
-        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5">
+        <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
           {skills.map((skill, index) => (
             <div 
               key={index}
-              className={`flex flex-col items-center justify-center p-4 ${
-                index === skills.length - 1 && 'col-span-2 sm:col-span-1 sm:col-start-3 lg:md:col-span-5'
-              }`}
+              className="flex flex-col items-center justify-center p-4 transition-transform duration-300 hover:scale-105"
             >
               <img 
                 src={skill.url} 
                 alt={skill.name} 
-                className="h-16 w-16 sm:h-20 sm:w-20 md:h-24 md:w-24" 
+                className="h-16 w-16 drop-shadow-lg sm:h-20 sm:w-20 md:h-24 md:w-24" 
               />
               <span className="mt-2 text-lg">{skill.name}</span>
             </div>
@@ -38,5 +38,5 @@ export default function StacksSection(){
         </div>
       </div>
     </section>
-  )
+  );
 }
