ef8d50d8bc6c15e0890c4ef77a8978ea1016d0bc
zThiagoR <thi.magalha13@gmail.com>
2023-12-29 15:23:01 -0300

    Add projectUrl to Project interface and update ProjectCard component

--- src/components/projectCard.tsx
+++ src/components/projectCard.tsx
@@ -1,42 +1,47 @@
-import { FaGithub } from 'react-icons/fa';
+import { FaGithub, FaExternalLinkAlt } from 'react-icons/fa';
+
 interface Project {
   title: string;
   description: string;
   tags: string[];
   sourceUrl: string;
+  projectUrl?: string; // URL opcional do site do projeto
 }
+
 interface ProjectCardProps {
   project: Project;
 }
 
-export default function ProjectCard({ project }: ProjectCardProps){
-  const { title, description, tags, sourceUrl } = project;
+export default function ProjectCard({ project }: ProjectCardProps) {
+  const { title, description, tags, sourceUrl, projectUrl } = project;
+  const isGithubDisabled = !sourceUrl;
+  const isProjectDisabled = !projectUrl;
 
   return (
-    <div className="mx-2 mb-4 grid grid-cols-1 rounded-lg bg-white p-6 shadow-md">
-      <h2 className="mb-2 text-xl font-bold text-black">{title}</h2>
-      <p className="mb-4 text-gray-600">{description}</p>
-      <div className="mb-4">
-        {tags.map((tag, index) => (
-          <span 
-            key={index} 
-            className="mx-1 rounded-full bg-gray-200 px-2 py-1 text-sm font-semibold text-gray-700"
-          >
-            {tag}
-          </span>
-        ))}
+    <div className="mx-2 mb-4 flex h-full cursor-pointer flex-col justify-between rounded-lg bg-button-primary p-6 shadow-lg transition duration-500 hover:scale-105 hover:shadow-xl">
+      <div>
+        <h2 className="mb-2 text-2xl font-bold">{title}</h2>
+        <p className="mb-4">{description}</p>
+        <div className="mb-4 flex flex-wrap">
+          {tags.map((tag, index) => (
+            <span 
+              key={index} 
+              className="mb-2 mr-2 rounded-full bg-button-secondary px-3 py-1 text-sm font-semibold text-white transition-colors duration-300 hover:brightness-125"
+            >
+              {tag}
+            </span>
+          ))}
+        </div>
       </div>
-      <div 
-        className={`flex items-center justify-center rounded-md bg-gray-200 py-2 text-center font-medium text-gray-700 transition-colors duration-300 hover:bg-gray-300
-${!sourceUrl && 'cursor-not-allowed opacity-50'}`}>
-        <a
-          href={sourceUrl}
-          target="_blank"
-          className={`${!sourceUrl && 'cursor-not-allowed'}`}
-        >
-          <FaGithub className="mr-2 inline-block text-xl" />
-          Ver no GitHub
-        </a>
+      <div className="flex space-x-2">
+        <div className={`${isGithubDisabled ? 'cursor-not-allowed opacity-50' : 'hover:brightness-125'} inline-flex flex-1 items-center justify-center rounded-md bg-button-secondary py-2 font-medium text-white transition-colors duration-300`}>
+          <FaGithub className="mr-2" />
+          <span>Ver no GitHub</span>
+        </div>
+        <div className={`${isProjectDisabled ? 'cursor-not-allowed opacity-50' : 'hover:brightness-125'} inline-flex flex-1 items-center justify-center rounded-md bg-button-secondary py-2 font-medium text-white transition-colors duration-300`}>
+          <FaExternalLinkAlt className="mr-2" />
+          <span>Visitar Site</span>
+        </div>
       </div>
     </div>
   );
