1100fd94d1b307793a5c74a8ea91b9b033fd809f
zThiagoR <thi.magalha13@gmail.com>
2025-10-03 00:56:10 -0300

    feat(sections): Add Experience section to main App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import Footer from "./components/footer";
 import Navbar from "./components/navbar";
 import AboutMe from "./components/sections/aboutme";
+import ExperienceSection from "./components/sections/experience";
 import ProjectsSection from "./components/sections/projects";
 import StacksSection from "./components/sections/stacks";
 
 function App() {
   return (
-    <main className="flex min-h-screen flex-col bg-secondary text-white">
+    <main className="min-h-screen bg-dark text-text-primary">
       <Navbar />
       <AboutMe />
+      <ExperienceSection />
       <StacksSection />
       <ProjectsSection />
       <Footer />
